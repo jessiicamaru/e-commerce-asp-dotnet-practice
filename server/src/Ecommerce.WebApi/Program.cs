93325412dcf9ea36c5a0deb2691d27d1913bfa00
jessiicamaru <Dandrew2407@gmail.com>
2026-08-31 14:33:56 +0700

    feat(webapi): register infrastructure services and add Postgres connection string

--- server/src/Ecommerce.WebApi/Program.cs
+++ server/src/Ecommerce.WebApi/Program.cs
@@ -1,8 +1,12 @@
+using Ecommerce.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
@@ -21,7 +25,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -39,3 +43,4 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
