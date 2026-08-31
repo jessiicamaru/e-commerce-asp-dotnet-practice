3a343b0e5a5bb7604373acc65a301b09e38b519c
jessiicamaru <Dandrew2407@gmail.com>
2026-08-31 17:32:13 +0700

    fix(webapi): restore app.Run() and add authentication/authorization middlewares in Program.cs

--- server/src/Ecommerce.WebApi/Program.cs
+++ server/src/Ecommerce.WebApi/Program.cs
@@ -4,7 +4,6 @@ using Ecommerce.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
@@ -20,4 +19,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
+
+app.Run();
