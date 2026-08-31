eaf6c24c206617ff011b28d9b3a6e8991806606c
jessiicamaru <Dandrew2407@gmail.com>
2026-08-31 17:43:31 +0700

    fix(domain): remove client-side Guid initialization from User and RefreshToken to fix EF Core relationship tracking

--- server/src/Ecommerce.Domain/Entities/RefreshToken.cs
+++ server/src/Ecommerce.Domain/Entities/RefreshToken.cs
@@ -2,7 +2,7 @@ namespace Ecommerce.Domain.Entities;
 
 public class RefreshToken
 {
-    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid Id { get; set; }
 
     public string Token { get; set; } = string.Empty;
 
--- server/src/Ecommerce.Domain/Entities/User.cs
+++ server/src/Ecommerce.Domain/Entities/User.cs
@@ -2,7 +2,7 @@ namespace Ecommerce.Domain.Entities;
 
 public class User
 {
-    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid Id { get; set; }
 
     public string Email { get; set; } = string.Empty;
 
