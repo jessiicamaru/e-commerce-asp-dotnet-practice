b31828af25b86d7b94a6612f6e6ccb8b2d3a2bcd
jessiicamaru <Dandrew2407@gmail.com>
2026-09-02 22:25:07 +0700

    feat(catalog): implement CreateProductCommandValidator rules

--- server/src/Services/Catalog/Ecommerce.Catalog.Application/Products/Commands/CreateProduct/CreateProductCommandValidator.cs
+++ server/src/Services/Catalog/Ecommerce.Catalog.Application/Products/Commands/CreateProduct/CreateProductCommandValidator.cs
@@ -6,12 +6,25 @@ public class CreateProductCommandValidator : AbstractValidator<CreateProductComm
 {
     public CreateProductCommandValidator()
     {
-        // TODO: Write validation rules for CreateProductCommand:
-        // - Name: NotEmpty(), MaximumLength(200)
-        // - Description: MaximumLength(2000) (if present)
-        // - Price: GreaterThan(0)
-        // - StockQuantity: GreaterThanOrEqualTo(0)
-        // - Sku: NotEmpty(), MaximumLength(50)
-        // - CategoryId: NotEmpty()
+
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Product name is required.")
+            .MaximumLength(200).WithMessage("Product name must be less than 200 characters.");
+
+        RuleFor(x => x.Description)
+            .MaximumLength(2000).WithMessage("Product description must be less than 2000 characters.");
+
+        RuleFor(x => x.Price)
+            .GreaterThan(0).WithMessage("Product price must be greater than 0.");
+
+        RuleFor(x => x.StockQuantity)
+            .GreaterThanOrEqualTo(0).WithMessage("Product stock quantity must be greater than or equal to 0.");
+
+        RuleFor(x => x.Sku)
+            .NotEmpty().WithMessage("Product SKU is required.")
+            .MaximumLength(50).WithMessage("Product SKU must be less than 50 characters.");
+
+        RuleFor(x => x.CategoryId)
+            .NotEmpty().WithMessage("Product category ID is required.");
     }
 }
